33dc11aa56b0f7c1911a29a33eace74b0b95afbf
Jerum Galang <jerumpgalang@gmail.com>
2020-08-07 21:11:49 +0800

    Finish Prod v2

--- client/src/components/TableResult/TableResult.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TableResult from './TableResult';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TableResult />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
